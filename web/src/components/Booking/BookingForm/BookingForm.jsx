97e6e3ff39ad5f85fd927c25974a1466f1393e88
Abdul Kareem <rajab8321@gmail.com>
2025-02-01 16:15:15 +0530

    version 2.0.1

--- web/src/components/Booking/BookingForm/BookingForm.jsx
+++ web/src/components/Booking/BookingForm/BookingForm.jsx
@@ -23,7 +23,7 @@ const BookingForm = (props) => {
 
   return (
     <div className="rw-form-wrapper">
-
+{/* hello */}
       <Form onSubmit={onSubmit} error={props.error}>
         <FormError
           error={props.error}
